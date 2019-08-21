bb13bb044c4282a13f0fafead428e7e034edefd5
LKingJ23 <lkingj23@gmail.com>
2019-08-21 15:02:41 +0200

    login simple

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../pages/usuario.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/';
+  private apikey = 'AIzaSyDqN3Wy9bJWLWd1CbQRayoAwdZdjjPigFg';
+  // Crear nuevo usuario
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  userToken: string;
+
+  constructor( private http: HttpClient) {
+    this.leerToken();
+   }
+
+  logout(){
+    localStorage.removeItem('token');
+
+  }
+
+  login(usuario: UsuarioModel){
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${this.url}accounts:signInWithPassword?key=${this.apikey}`,
+      authData
+    ).pipe(
+      map(res => {
+        this.guardarToken(res['idToken']);
+        return res;
+      })
+    );
+
+  }
+
+  nuevoUsuario(usuario: UsuarioModel){
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${this.url}accounts:signUp?key=${this.apikey}`,
+      authData
+    ).pipe(
+      map(res => {
+        this.guardarToken(res['idToken']);
+        return res;
+      })
+    );
+
+  }
+
+  private guardarToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+    localStorage.setItem('expira', hoy.getTime().toString());
+
+  }
+
+  leerToken(){
+    if (localStorage.getItem('token')){
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
+  }
+
+  estaAutenticado(): boolean {
+    if(this.userToken.length < 2){
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if(expiraDate > new Date()){
+      return true;
+    } else {
+      return false;
+    }
+
+    return this.userToken.length > 2;
+  }
+}
